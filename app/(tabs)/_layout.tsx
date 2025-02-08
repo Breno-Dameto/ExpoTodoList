ce1af41aa5df32e2494c80b3b5059b20da3ef23c
Breno Dameto <163887566+bDametoS@users.noreply.github.com>
2025-02-07 23:06:01 -0300

    tabs icon and name change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { Ionicons } from '@expo/vector-icons';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -29,15 +28,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Tasks',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="list-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Done',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="checkmark-circle-outline" color={color} />,
         }}
       />
     </Tabs>
